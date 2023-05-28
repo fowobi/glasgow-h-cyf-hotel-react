38cb12efffbf406cff6d0b325e09255193f4848e
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-28 22:55:35 +0100

    create and update TouristInfocards.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import TouristInfoCards from './components/TouristInfoCards';
 import Bookings from './components/Bookings'
 import './App.css'
 
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">CYF Hotel</header>
+       <TouristInfoCards />
       <Bookings />
     </div>
   )
