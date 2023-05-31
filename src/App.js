58e8c31953bb61c8f74beba05d2a7348d3de60ce
Man Sang Sin <man.sang.sin@gmail.com>
2023-05-31 14:31:15 +0100

    created and updated Heading.js
    Removed Heading section from App.js and created a separate component from the code removed.
    Imported component into App.js.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-
-import Bookings from './components/Bookings'
-import './App.css'
+import React from "react";
+import Bookings from "./components/Bookings";
+import Heading from "./components/Heading";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Heading />
       <Bookings />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
