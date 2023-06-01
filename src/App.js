2997fb3f5e884dc80796cb03225dd63c0e8d4fb9
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-06-01 10:24:42 +0100

    Fixed bug after merging Heading component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import TouristInfoCards from './components/TouristInfoCards';
+import TouristInfoCards from './components/TouristInfoCards'
 import Bookings from './components/Bookings'
 import './App.css'
 import SearchResult from './components/SearchResult'
+import Heading from './components/Heading'
 
 const App = () => {
   return (
-    <>
+    <div>
       <Heading />
-       <TouristInfoCards />
+      <TouristInfoCards />
       <Bookings />
       <SearchResult />
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default App;
+export default App
