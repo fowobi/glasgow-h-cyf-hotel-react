92972f7bf3c6907528f959ab25ffe76e92ec0dd4
Man Sang Sin <man.sang.sin@gmail.com>
2023-06-03 12:09:46 +0100

    imported Heading from Heading.js component
    fixed app not loading correctly. Heading component was not imported before being used.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import TouristInfoCards from './components/TouristInfoCards';
-import Bookings from './components/Bookings'
-import './App.css'
-import SearchResult from './components/SearchResult'
+import React from "react";
+import TouristInfoCards from "./components/TouristInfoCards";
+import Bookings from "./components/Bookings";
+import "./App.css";
+import SearchResult from "./components/SearchResult";
+import Heading from "./components/Heading";
 
 const App = () => {
   return (
     <>
       <Heading />
-       <TouristInfoCards />
+      <TouristInfoCards />
       <Bookings />
       <SearchResult />
     </>
