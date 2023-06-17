3db309a99c9c18872df6f49b540833b065594ff8
fowobi <109865505+fowobi@users.noreply.github.com>
2023-06-17 22:27:06 +0100

    Update Bookings.js

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,10 +1,18 @@
+import React, { useState,useEffect} from "react";
 import Search from "./Search.js";
 import SearchResult from "./SearchResult.js";
-import FakeBookings from "../data/fakeBookings";
-import React, { useState } from "react";
+// import FakeBookings from "../data/fakeBookings";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    console.log("Page first renders on the screen.");
+
+    fetch("https://cyf-react.glitch.me")
+      .then((response) => response.json())
+      .then((data) => setBookings(data));
+  }, []);
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
@@ -14,10 +22,10 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResult results={bookings} />
+        <SearchResult results={bookings}/>
       </div>
     </div>
   );
-};
+}
 
 export default Bookings;
