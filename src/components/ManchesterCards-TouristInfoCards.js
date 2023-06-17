c5fa47341045c970dd7ef875a6445c9f7aba150c
fowobi <109865505+fowobi@users.noreply.github.com>
2023-06-17 12:40:13 +0100

    created cards.js and update touristinfocard.js

--- src/components/ManchesterCards-TouristInfoCards.js
+++ src/components/ManchesterCards-TouristInfoCards.js
@@ -2,29 +2,29 @@ import React from "react";
 
 const ManchesterCards = () => {
   return (
-    <div className="card">
-      <img
-        src="https://eu-assets.simpleview-europe.com/manchester2016/imageresizer/?image=%2Fdbimgs%2FManOne-03%284%29.jpg&action=Background_Overlay"
-        className="card-img-top"
-        alt="Manchester"
-      />
+//     <div className="card">
+//       <img
+//         src="https://eu-assets.simpleview-europe.com/manchester2016/imageresizer/?image=%2Fdbimgs%2FManOne-03%284%29.jpg&action=Background_Overlay"
+//         className="card-img-top"
+//         alt="Manchester"
+//       />
 
-      <div className="card-body">
-        <h1>Manchester</h1>
-        <p>
-          Experience the dynamic city of Manchester with its bustling music
-          scene and iconic landmarks.
-        </p>
+//       <div className="card-body">
+//         <h1>Manchester</h1>
+//         <p>
+//           Experience the dynamic city of Manchester with its bustling music
+//           scene and iconic landmarks.
+//         </p>
 
-        <a
-          href="https://visitmanchester.com"
-          target="_blank"
-          className="btn btn-primary">
-          Visit Website
-        </a>
-      </div>
-    </div>
-  );
-};
+//         <a
+//           href="https://visitmanchester.com"
+//           target="_blank"
+//           className="btn btn-primary">
+//           Visit Website
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ManchesterCards;
+// export default ManchesterCards;
--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import GlasgowCards from './GlasgowCards-TouristInfoCards'
-import ManchesterCards from './ManchesterCards-TouristInfoCards'
-import LondonCards from './LondonCard-TouristInfoCards'
+// import React from 'react'
+// import GlasgowCards from './GlasgowCards-TouristInfoCards'
+// import ManchesterCards from './ManchesterCards-TouristInfoCards'
+// import LondonCards from './LondonCard-TouristInfoCards'
+
+// const TouristInfoCards = () => {
+//   return (
+//     <div className="cards-container">
+//       <GlasgowCards />
+//       <ManchesterCards />
+//       <LondonCards />
+//     </div>
+//   )
+// }
+
+// export default TouristInfoCards
+
+
+import React from "react";
+import Cards from "./Cards";
 
 const TouristInfoCards = () => {
   return (
     <div className="cards-container">
-      <GlasgowCards />
-      <ManchesterCards />
-      <LondonCards />
+      <Cards />
     </div>
-  )
-}
+  );
+};
 
-export default TouristInfoCards
+export default TouristInfoCards;
